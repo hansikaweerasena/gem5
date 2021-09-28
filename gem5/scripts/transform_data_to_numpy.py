7c2edd014a9533a550eec2841bbb66840fc8a503
Hansika <hansikam94@gmail.com>
2021-09-28 14:51:53 -0400

    reduce convertion of dataset to numpy

--- gem5/scripts/transform_data_to_numpy.py
+++ gem5/scripts/transform_data_to_numpy.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import numpy as np
+from random import randint
 
 NUMBER_OF_NODES = "64_nodes"
 BASE_PATH = "/home/hansika/gem5/gem5/scripts/"
@@ -10,6 +11,7 @@ NUMPY_DATA_DIR_PATH = BASE_PATH + "numpy_data/"
 
 NUM_ITERATIONS_PER_FILE = 100
 MIN_MAX_LENGTH = 450
+calculate_reduced = True
 
 
 def create_dir(path):
@@ -61,6 +63,33 @@ def convert_to_numpy(up_flit_ipd, down_flit_ipd, node1, node2, no_of_nodes, nump
                 numpy_for_dir.append(np.array(flow_pair))
                 correlation_dir.append(correlation)
 
+def generate_random(range_start, range_end, exclude_list):
+    rand_node = randint(range_start,range_end)
+    while(rand_node in exclude_list):
+        rand_node = randint(range_start,range_end)
+    return rand_node
+
+def convert_to_numpy_reduced(up_flit_ipd, down_flit_ipd, node1, node2, no_of_nodes, numpy_for_dir, correlation_dir):
+    cur_node_list = [node1,node2]
+    node3 = generate_random(0, no_of_nodes - 1, cur_node_list) 
+    cur_node_list.append(node3)
+    node4 = generate_random(0, no_of_nodes - 1, cur_node_list)
+
+    convert_to_numpy_local(up_flit_ipd.get(node1), down_flit_ipd.get(node2 + no_of_nodes), 1, numpy_for_dir, correlation_dir)
+    convert_to_numpy_local(up_flit_ipd.get(node2), down_flit_ipd.get(node1 + no_of_nodes), 1, numpy_for_dir, correlation_dir)
+    convert_to_numpy_local(up_flit_ipd.get(node1), down_flit_ipd.get(node3 + no_of_nodes), 0, numpy_for_dir, correlation_dir)
+    convert_to_numpy_local(up_flit_ipd.get(node4), down_flit_ipd.get(node2 + no_of_nodes), 0, numpy_for_dir, correlation_dir)
+
+
+
+def convert_to_numpy_local(up_value, down_value, correlation, numpy_for_dir, correlation_dir):
+    up_flow = np.array(up_value)
+    down_flow = np.array(down_value)
+    up_flow.resize(MIN_MAX_LENGTH, refcheck=False)
+    down_flow.resize(MIN_MAX_LENGTH, refcheck=False)
+    flow_pair = [up_flow, down_flow]
+    numpy_for_dir.append(np.array(flow_pair))
+    correlation_dir.append(correlation)
 
 def process_sythetic_traffic_count(frm, to, syntheticTrafficCount):
     key = frm + "->" + to
@@ -131,11 +160,20 @@ def process_file(filename, numpy_for_dir, correlation_dir):
         for line in f:
             process_line(line, niPacketCount, syntheticTrafficCount, flitCount, down_flit_ipd, up_flit_ipd, no_of_nodes)
     write_to_file(fileN, str(node1) + "_" + str(node2), syntheticTrafficCount, niPacketCount, flitCount)
-    convert_to_numpy(up_flit_ipd, down_flit_ipd, node1, node2, no_of_nodes, numpy_for_dir, correlation_dir)
+    if calculate_reduced:
+        convert_to_numpy_reduced(up_flit_ipd, down_flit_ipd, node1, node2, no_of_nodes, numpy_for_dir, correlation_dir)
+    else:
+        convert_to_numpy(up_flit_ipd, down_flit_ipd, node1, node2, no_of_nodes, numpy_for_dir, correlation_dir)
+
 
 def save_numpy_array(numpy_for_dir, correlation_dir, index):
     np.save(os.path.join(NUMPY_DATA_DIR_PATH + NUMBER_OF_NODES + "/X", index), np.array(numpy_for_dir))
     np.save(os.path.join(NUMPY_DATA_DIR_PATH + NUMBER_OF_NODES + "/Y", index), np.array(correlation_dir))
+
+
+
+if calculate_reduced:
+    NUMPY_DATA_DIR_PATH = BASE_PATH + "numpy_data_reduced/"
 
 list_subdir_with_paths = [f.path for f in os.scandir(RAW_DATA_DIR_PATH + NUMBER_OF_NODES + "/") if f.is_dir()]
 print(RAW_DATA_DIR_PATH + NUMBER_OF_NODES + "/")
@@ -153,7 +191,6 @@ for sub_dir in list_subdir_with_paths:
         process_file(filename, numpy_for_dir, correlation_dir)
         i += 1
         if i == NUM_ITERATIONS_PER_FILE:
-            x = np.array(numpy_for_dir)
             save_numpy_array(numpy_for_dir, correlation_dir, str(j))
             j += 1
             i = 1
