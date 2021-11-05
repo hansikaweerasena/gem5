a9434aaf99cc7bce3c79b3a5da3407765265a8b1
Hansika <hansikam94@gmail.com>
2021-11-05 10:42:01 -0400

    adding delay as command line arg

--- gem5/scripts/collect_data.sh
+++ gem5/scripts/collect_data.sh
@@ -2,22 +2,51 @@ export nodes=$1
 export rows=$2
 export iterations=$3
 export simCycles=$4
+export dest_folder=$5
+export traffic_prob=$6
+export apply_cf=$7
+export apply_delay=$8
 
 echo "Running traffic corelation data for and rows : " $nodes " " $rows
+
+export is_apply_cf=false
+export cw=""
+export cw_flag=""
+if [[ "$apply_cf" == "-cw" ]]; then
+    is_apply_cf=true 
+    cw="c"
+    cw_flag="--enable-add-chaff"
+fi
+
+export delay=""
+export delay_flag=""
+if [[ "$apply_delay" == "-delay" ]]; then
+    delay="d"
+    delay_flag="--enable-add-delay"
+fi
+
 
 for i in $( eval echo {0..$(($nodes-1))})
 do
     for j in $( eval echo {0..$(($nodes-1))})
     do
+        if [[ ("$is_apply_cf" = true ) && (($j -eq $(($i-1))) || ($j -eq $(($i+1))) || ($j -eq $(($i+$rows))) || ($j -eq $(($i-$rows))))]]; then
+            continue
+        fi
         if [ $i -eq $j ]; then
             continue
         fi
         for k in $( eval echo {0..$(($iterations-1))})
         do
             export out_filename="${nodes}_${i}_${j}_${k}.txt" 
-            ../build/X86_DeepCorr/gem5.debug -d raw_data/"${nodes}_nodes_100"/${i}_${j} --debug-file=$out_filename --debug-flags=GarnetSyntheticTraffic2,Hello ../configs/example/garnet_synth_traffic.py --cor-p1=$i --cor-p2=$j --num-cpus=$nodes --num-dirs=$nodes --network=garnet2.0 --topology=Mesh_XY --mesh-rows=$rows --sim-cycles=$simCycles  --synthetic=uniform_random --injectionrate=0.01
+            ../build/X86_DeepCorr/gem5.debug -d $dest_folder/"${nodes}_nodes_${traffic_prob}_${cw}${delay}"/${i}_${j} --debug-file=$out_filename --debug-flags=GarnetSyntheticTraffic2,Hello ../configs/example/garnet_synth_traffic.py --cor-p1=$i --cor-p2=$j --cor-prec=$traffic_prob --num-cpus=$nodes --num-dirs=$nodes --network=garnet2.0 --topology=Mesh_XY --mesh-rows=$rows --sim-cycles=$simCycles  --synthetic=uniform_random --injectionrate=0.01  $cw_flag  $delay_flag
         done
-        rm -r raw_data/"${nodes}_nodes"/${i}_${j}/fs
+        rm -r $dest_folder/"${nodes}_nodes_${traffic_prob}_${cw}${delay}"/${i}_${j}/fs
+        rm -r $dest_folder/"${nodes}_nodes_${traffic_prob}_${cw}${delay}"/${i}_${j}/*.dot
+        rm -r $dest_folder/"${nodes}_nodes_${traffic_prob}_${cw}${delay}"/${i}_${j}/*.pdf
+        rm -r $dest_folder/"${nodes}_nodes_${traffic_prob}_${cw}${delay}"/${i}_${j}/*.svg
+        rm -r $dest_folder/"${nodes}_nodes_${traffic_prob}_${cw}${delay}"/${i}_${j}/*.ini
+        rm -r $dest_folder/"${nodes}_nodes_${traffic_prob}_${cw}${delay}"/${i}_${j}/*.json
     done
 done
 
